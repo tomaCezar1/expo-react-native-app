60c72c18afe063d36012710266efe5c403f7e548
tomaCezar1 <71145334+tomacezar1@users.noreply.github.com>
2026-04-15 10:18:45 +0300

    new component

---
+++ app/onboarding.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+const Onboarding = () => {
+  return (
+    <View>
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+};
+
+export default Onboarding;
