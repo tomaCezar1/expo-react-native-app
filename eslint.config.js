dc42e595fbaa0d09199e3ea17c3b6ce6dc89c7a4
tomaCezar1 <71145334+tomacezar1@users.noreply.github.com>
2026-04-23 11:09:04 +0300

    config changes

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,21 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ['dist/*'],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.json',
+        },
+        alias: {
+          map: [['@', './src']],
+          extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        },
+      },
+    },
   },
 ]);
--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,15 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativewind } = require('nativewind/metro');
 
 /** @type {import('expo/metro-config').MetroConfig} */
+
+const path = require('path');
 const config = getDefaultConfig(__dirname);
+
+// Add alias for '@' to match tsconfig.json
+config.resolver = config.resolver || {};
+config.resolver.alias = {
+  ...(config.resolver.alias || {}),
+  '@': path.resolve(__dirname, 'src'),
+};
 
 module.exports = withNativewind(config);
