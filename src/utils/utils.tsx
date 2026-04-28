8dbfc4385061968298cb7e66eb88e0a0d2009b34
tomaCezar1 <71145334+tomacezar1@users.noreply.github.com>
2026-04-28 10:05:51 +0300

    add formatCurrency util

---
+++ src/utils/utils.tsx
@@ -0,0 +1,12 @@
+export const formatCurrency = (value: number, currency = 'USD'): string => {
+  try {
+    return new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency,
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(value);
+  } catch {
+    return value.toFixed(2);
+  }
+};
