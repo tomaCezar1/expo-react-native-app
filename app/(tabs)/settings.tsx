0492030b3066fcc1994501118b927166c348d9bb
tomaCezar1 <71145334+tomacezar1@users.noreply.github.com>
2026-04-29 11:41:16 +0300

    create settings component

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,8 +1,14 @@
+import { styled } from 'nativewind';
+import { Text } from 'react-native';
+import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
+
+const SafeAreaView = styled(RNSafeAreaView);
+
 const Settings = () => {
   return (
-    <div>
-      <h1>Settings</h1>
-    </div>
+    <SafeAreaView className="flex-1 bg-background p-5">
+      <Text className="text-xl font-bold text-success">Settings</Text>
+    </SafeAreaView>
   );
 };
 
