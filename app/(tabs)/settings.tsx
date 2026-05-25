84d791a355945fe83d5f182f81b284aed03e5182
tomaCezar1 <71145334+tomacezar1@users.noreply.github.com>
2026-05-25 11:49:13 +0300

    add capture for sign out

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,6 +1,7 @@
 import images from '@/constants/images';
 import { useClerk, useUser } from '@clerk/expo';
 import { styled } from 'nativewind';
+import { usePostHog } from 'posthog-react-native';
 import { Image, Pressable, Text, View } from 'react-native';
 import { SafeAreaView as RNSafeAreaView } from 'react-native-safe-area-context';
 
@@ -9,9 +10,12 @@ const SafeAreaView = styled(RNSafeAreaView);
 const Settings = () => {
   const { signOut } = useClerk();
   const { user } = useUser();
+  const posthog = usePostHog();
 
   const handleSignOut = async () => {
     try {
+      posthog.capture('user_signed_out');
+      posthog.reset();
       await signOut();
     } catch (error) {
       console.error('Sign-out failed:', error);
